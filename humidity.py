f44d1a3984f1e5b079667b3354a9c2b44fab2a97
Jacques <jba@zengularity.com>
2014-11-28 16:10:01 +0100

    Change the humidity port

--- humidity.py
+++ humidity.py
@@ -18,18 +18,18 @@ def bin2dec(string_num):
 while True:
   time.sleep(0.5)
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(4,GPIO.OUT)
-  GPIO.output(4,GPIO.HIGH)
+  GPIO.setup(21,GPIO.OUT)
+  GPIO.output(21,GPIO.HIGH)
   time.sleep(0.025)
-  GPIO.output(4,GPIO.LOW)
+  GPIO.output(21,GPIO.LOW)
   time.sleep(0.02)
 
-  GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
   data = []
 
   for i in range(0,500):
-      data.append(GPIO.input(4))
+      data.append(GPIO.input(21))
 
   bit_count = 0
   tmp = 0
