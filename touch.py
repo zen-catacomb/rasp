498818857f64e2b31909569215c9065409486f65
Jacques <jba@zengularity.com>
2014-11-28 15:53:01 +0100

    Add script for touch

---
+++ touch.py
@@ -0,0 +1,26 @@
+import urllib
+import RPi.GPIO as GPIO
+import time
+import sys
+
+url='http://zen-catacomb.herokuapp.com/touch/'
+nodata = "".encode("UTF-8")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+def log(msg):
+    sys.stdout.write( time.strftime("%H:%M:%S") + " - " + msg + "\n")
+    sys.stdout.flush()
+
+prev = 0
+while True:
+    try:
+        value = GPIO.input(4)
+        if( value == 0 and prev == 1 ):
+          log("touch event")
+          urllib.urlopen(url, data=nodata)
+        prev = value
+    except Exception as e:
+        log('x')
+    time.sleep(0.1)
