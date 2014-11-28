32f43674cc7826ede81e222b0560f11008098a6a
Jacques <jba@zengularity.com>
2014-11-28 14:39:34 +0100

    Work on humidity

--- humidity.py
+++ humidity.py
@@ -1,10 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+import urllib
+
+urlHum ='http://zen-catacomb.herokuapp.com/humidity/'
+urlTemp='http://zen-catacomb.herokuapp.com/temperature/'
+nodata = "".encode("UTF-8")
+
+def log(msg):
+    sys.stdout.write(time.strftime("%H:%M:%S") + msg + "\n")
+    sys.stdout.flush()
 
 def bin2dec(string_num):
     return str(int(string_num, 2))
-
-data = []
 
 GPIO.setmode(GPIO.BCM)
 
@@ -16,72 +23,77 @@ time.sleep(0.02)
 
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-for i in range(0,500):
-    data.append(GPIO.input(4))
+while True:
+  data = []
 
-bit_count = 0
-tmp = 0
-count = 0
-HumidityBit = ""
-TemperatureBit = ""
-crc = ""
+  for i in range(0,500):
+      data.append(GPIO.input(4))
 
-try:
-  while data[count] == 1:
-    tmp = 1
-    count = count + 1
+  bit_count = 0
+  tmp = 0
+  count = 0
+  HumidityBit = ""
+  TemperatureBit = ""
+  crc = ""
 
-  for i in range(0, 32):
-    bit_count = 0
-
-    while data[count] == 0:
+  try:
+    while data[count] == 1:
       tmp = 1
       count = count + 1
 
-    while data[count] == 1:
-      bit_count = bit_count + 1
-      count = count + 1
+    for i in range(0, 32):
+      bit_count = 0
 
-    if bit_count > 3:
-      if i>=0 and i<8:
-        HumidityBit = HumidityBit + "1"
-      if i>=16 and i<24:
-        TemperatureBit = TemperatureBit + "1"
-    else:
-      if i>=0 and i<8:
-        HumidityBit = HumidityBit + "0"
-      if i>=16 and i<24:
-        TemperatureBit = TemperatureBit + "0"
+      while data[count] == 0:
+        tmp = 1
+        count = count + 1
 
-except:
-  print("ERR_RANGE")
-  exit(0)
+      while data[count] == 1:
+        bit_count = bit_count + 1
+        count = count + 1
 
-try:
-  for i in range(0, 8):
-    bit_count = 0
+      if bit_count > 3:
+        if i>=0 and i<8:
+          HumidityBit = HumidityBit + "1"
+        if i>=16 and i<24:
+          TemperatureBit = TemperatureBit + "1"
+      else:
+        if i>=0 and i<8:
+          HumidityBit = HumidityBit + "0"
+        if i>=16 and i<24:
+          TemperatureBit = TemperatureBit + "0"
 
-    while data[count] == 0:
-      tmp = 1
-      count = count + 1
+  except:
+    log("ERR_RANGE")
+    exit(0)
 
-    while data[count] == 1:
-      bit_count = bit_count + 1
-      count = count + 1
+  try:
+    for i in range(0, 8):
+      bit_count = 0
 
-    if bit_count > 3:
-      crc = crc + "1"
-    else:
-      crc = crc + "0"
-except:
-  print("ERR_RANGE")
-  exit(0)
+      while data[count] == 0:
+        tmp = 1
+        count = count + 1
 
-Humidity = bin2dec(HumidityBit)
-Temperature = bin2dec(TemperatureBit)
+      while data[count] == 1:
+        bit_count = bit_count + 1
+        count = count + 1
 
-if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
-  print("Humidity:"+ Humidity +"%")
-  print("Temperature:"+ Temperature +"C")
-else:
-  print("ERR_CRC")
+      if bit_count > 3:
+        crc = crc + "1"
+      else:
+        crc = crc + "0"
+  except:
+    log("ERR_RANGE")
+    exit(0)
+
+  Humidity = bin2dec(HumidityBit)
+  Temperature = bin2dec(TemperatureBit)
+
+  if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
+    log("Humidity:"+ Humidity +"%")
+    #urllib.urlopen(urlHum + Humidity, data=nodata, timeout=1)
+    log("Temperature:"+ Temperature +"C")
+    #urllib.urlopen(urlTemp + Temperature, data=nodata, timeout=1)
+  else:
+    log("ERR_CRC")
