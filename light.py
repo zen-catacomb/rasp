af552f47b14b664d42a89c103535c8dfe62d630e
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-28 13:45:19 +0100

    make light use GPIO

--- light.py
+++ light.py
@@ -1,13 +1,13 @@
 import urllib
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import sys
 
 url='http://10.0.25.240:8080/light/'
 nodata = "".encode("UTF-8")
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(4,GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
 
 def log(msg):
     sys.stdout.write('.')
@@ -15,10 +15,9 @@ def log(msg):
 
 while True:
     try:
-        # value=str(GPIO.input(4))
-        value = "1"
-        urllib.urlopen(url + value, data=nodata, timeout=1)
+        value=str(GPIO.input(4))
         log(value)
+        urllib.urlopen(url + value, data=nodata, timeout=1)
     except Exception as e:
         log('x')
     time.sleep(1)
