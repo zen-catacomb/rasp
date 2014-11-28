17502443d3301462b8735884df9f5f705bd8cee8
Jacques <jba@zengularity.com>
2014-11-28 17:13:57 +0100

    Add the sound

---
+++ sound.py
@@ -0,0 +1,23 @@
+import urllib
+import RPi.GPIO as GPIO
+import time
+import sys
+
+url='http://zen-catacomb.herokuapp.com/sound/'
+nodata = "".encode("UTF-8")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24,GPIO.IN)
+
+def log(msg):
+    sys.stdout.write( time.strftime("%H:%M:%S") + " - " + msg + "\n")
+    sys.stdout.flush()
+
+while True:
+    try:
+        value=str(1-GPIO.input(24))
+        log(value)
+        urllib.urlopen(url + value, data=nodata)
+    except Exception as e:
+        log('x')
+    time.sleep(1)
